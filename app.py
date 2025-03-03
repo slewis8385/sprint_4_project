c38733c92141031740368095ab6116ebf930b048
slewis8385 <lewis8385@gmail.com>
2025-03-03 12:46:10 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -9,3 +9,4 @@ if st.checkbox("Show only cars under $20,000"):
     df = df[df['price'] < 20000]
 st.write(px.histogram(df, x='model', y='price', nbins= 20))
 st.write(px.scatter(df, x='model', y='price'))
+st.write(px.scatter(df, x='model', y='price', title='Price vs. Model', log_y=True))
